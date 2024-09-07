1a833d6d448e301e16af151ce7cb1f4bc30023c4
Samuel Theising <theisingsamuel@gmail.com>
2024-09-06 20:08:00 -0400

    Directory Fix
    Fixed where it would get mad and say I was in the wrong directory

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
-os.chdir(r"E:\49500Capstone\49500")  # Change to the directory where your CSV file is
+ # Change to the directory where your CSV file is
 # Print the current working directory
 print("Current Working Directory:", os.getcwd())
 
@@ -9,8 +9,8 @@ import warnings
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-input_file = r"test_data.csv"
-output_file = r"test_data_tokens.csv"
+input_file = r"BBC_train_full.csv"
+output_file = r"BBC_train_full_tokens.csv"
 
 nlp = spacy.load("en_core_web_sm")
 with open(input_file, 'r', newline='', encoding='utf-8') as infile, \
@@ -22,12 +22,12 @@ with open(input_file, 'r', newline='', encoding='utf-8') as infile, \
     # Iterate through each row in the CSV
     for row in reader:
         # Assume the text to tokenize is in the first column
-        text = row[0]
+        text = row[1]
 
         # Process the text using spaCy
         doc = nlp(text)
         # Remove stop words
-        filtered_tokens = [token.text for token in doc if not token.is_stop and not token.is_punct and len(token.text.strip()) > 1 ]
+        filtered_tokens = [token.text for token in doc if not token.is_stop and not token.is_punct and (len(token.text.strip()) > 1 or token.text.isdigit())]
 
 
         # Print the text excluding stop words
