c2ee5b40fa656983aa1a3e63a5372a3b99a64028
Samuel Theising <theisingsamuel@gmail.com>
2024-09-06 21:34:26 -0400

    Comment Updates

--- check_tokens.py
+++ check_tokens.py
@@ -2,18 +2,21 @@ import re
 import os
 import csv
 
+# Regex pattern to check for words under 2 digits, or anything not fitting once its finished.
 pattern = r'([a-zA-Z]{2,})|[\d]+|[¬£]\d+'
 
 
 input_file = r"test_data_tokens.csv"
 
+# Function for checking against the regex
 def check_string(input_string):
+    # Outputs if not matching, ignores if it is.
     if not re.match(pattern, input_string):
         print(f"'{input_string}' does not match the pattern")
         
 with open(input_file, 'r', newline='', encoding='utf-8') as infile:
         reader = csv.reader(infile)
-
+        # Iterates through each cell in a row, before moving on to the next row.
         for row in reader:
             for cell in row:  # Check every cell in every row
                 check_string(cell)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
- # Change to the directory where your CSV file is
+
 # Print the current working directory
 print("Current Working Directory:", os.getcwd())
 
@@ -15,21 +15,24 @@ output_file = r"BBC_train_full_tokens.csv"
 nlp = spacy.load("en_core_web_sm")
 with open(input_file, 'r', newline='', encoding='utf-8') as infile, \
     open(output_file, 'w', newline = '', encoding = 'utf-8') as outfile :
-    # Create a CSV reader object
+    # Create a CSV reader + writer object
     reader = csv.reader(infile)
     writer = csv.writer(outfile)
 
     # Iterate through each row in the CSV
     for row in reader:
-        # Assume the text to tokenize is in the first column
+        # Assume the text to tokenize is in the correct column
+        #Change to 0 for test data, 1 for BBC(labels are in column 0)
         text = row[1]
 
         # Process the text using spaCy
         doc = nlp(text)
-        # Remove stop words
+        # Remove stop words, punctuation, and words under 2 digits.
         filtered_tokens = [token.text for token in doc if not token.is_stop and not token.is_punct and (len(token.text.strip()) > 1 or token.text.isdigit())]
 
 
-        # Print the text excluding stop words
+        # output post-filtering to new .csv file
         writer.writerow(filtered_tokens)
+        
+        
 print(f"Processed data has been written to {output_file}")
