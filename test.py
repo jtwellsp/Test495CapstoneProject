2924f5440b040fbf6f9bb96d25702276d5fa9203
jtwellsp <jtwellspring12@gmail.com>
2024-09-07 00:03:44 -0400

    Added in some fixes to test.py

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ print("Current Working Directory:", os.getcwd())
 import csv
 import spacy
 import warnings
+import pandas as pd
+import re
+from nltk.tokenize import word_tokenize
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
@@ -13,8 +16,18 @@ input_file = r"BBC_train_full.csv"
 output_file = r"BBC_train_full_tokens.csv"
 
 nlp = spacy.load("en_core_web_sm")
+
+def preprocess_text(text):
+    # Remove digits using regex
+    text = re.sub(r'£\d+(?:\.\d+)?|€\d+(?:\.\d+)?|\$\d+(?:\.\d+)?', '', text)
+    text = re.sub(r'\b\d+(\.\d+)?\b', '', text)
+    text = text.replace('-', '')
+    text = text.replace("'", '')
+    text = text.replace(".", '')
+    return text
+
 with open(input_file, 'r', newline='', encoding='utf-8') as infile, \
-    open(output_file, 'w', newline = '', encoding = 'utf-8') as outfile :
+    open(output_file, 'w', newline = '', encoding = 'utf-8') as outfile:
     # Create a CSV reader + writer object
     reader = csv.reader(infile)
     writer = csv.writer(outfile)
@@ -25,14 +38,16 @@ with open(input_file, 'r', newline='', encoding='utf-8') as infile, \
         #Change to 0 for test data, 1 for BBC(labels are in column 0)
         text = row[1]
 
+        preprocessed_text = preprocess_text(text)
         # Process the text using spaCy
-        doc = nlp(text)
+        doc = nlp(preprocessed_text)
         # Remove stop words, punctuation, and words under 2 digits.
-        filtered_tokens = [token.text for token in doc if not token.is_stop and not token.is_punct and (len(token.text.strip()) > 1 or token.text.isdigit())]
+        filtered_tokens = [token.text.strip() for token in doc if not token.is_stop and not token.is_punct and not token.text.isdigit()]
 
-
+        filtered_tokens = [token for token in filtered_tokens if token]
         # output post-filtering to new .csv file
-        writer.writerow(filtered_tokens)
+        if filtered_tokens:
+            writer.writerow(filtered_tokens)
         
         
 print(f"Processed data has been written to {output_file}")
