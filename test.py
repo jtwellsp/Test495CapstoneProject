c2e8e192d0e84018074475cb6d2607c513360a3d
jtwellsp <jtwellspring12@gmail.com>
2024-09-07 21:38:16 -0400

    fix labels and add back in numbers

--- test.py
+++ test.py
@@ -1,14 +1,8 @@
-import os
-
-# Print the current working directory
-print("Current Working Directory:", os.getcwd())
 
 import csv
 import spacy
 import warnings
-import pandas as pd
 import re
-from nltk.tokenize import word_tokenize
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
@@ -18,36 +12,40 @@ output_file = r"BBC_train_full_tokens.csv"
 nlp = spacy.load("en_core_web_sm")
 
 def preprocess_text(text):
-    # Remove digits using regex
-    text = re.sub(r'£\d+(?:\.\d+)?|€\d+(?:\.\d+)?|\$\d+(?:\.\d+)?', '', text)
-    text = re.sub(r'\b\d+(\.\d+)?\b', '', text)
+    # Remove currency values 
+    # text = re.sub(r'£\d+(?:\.\d+)?|€\d+(?:\.\d+)?|\$\d+(?:\.\d+)?', '', text)
+    # Replace hyphens with spaces 
     text = text.replace('-', '')
+    # Replace apostrophes with spaces 
     text = text.replace("'", '')
-    text = text.replace(".", '')
     return text
 
 with open(input_file, 'r', newline='', encoding='utf-8') as infile, \
-    open(output_file, 'w', newline = '', encoding = 'utf-8') as outfile:
-    # Create a CSV reader + writer object
+    open(output_file, 'w', newline='', encoding='utf-8') as outfile:
+    # Create a CSV reader & writer object
     reader = csv.reader(infile)
     writer = csv.writer(outfile)
 
     # Iterate through each row in the CSV
     for row in reader:
         # Assume the text to tokenize is in the correct column
-        #Change to 0 for test data, 1 for BBC(labels are in column 0)
         text = row[1]
 
+        # Preprocess the text
         preprocessed_text = preprocess_text(text)
+
         # Process the text using spaCy
         doc = nlp(preprocessed_text)
-        # Remove stop words, punctuation, and words under 2 digits.
-        filtered_tokens = [token.text.strip() for token in doc if not token.is_stop and not token.is_punct and not token.text.isdigit()]
 
-        filtered_tokens = [token for token in filtered_tokens if token]
-        # output post-filtering to new .csv file
+        # Filter tokens retain numbers, remove single letters, stopwords, and punctuation
+        filtered_tokens = [token.text for token in doc if not token.is_stop and not token.is_punct and (token.like_num or len(token.text) > 1)  #
+        ]
+        
+        # Remove empty tokens
+        filtered_tokens = [token for token in filtered_tokens if token.strip()]
+
+        # Write non-empty filtered tokens to the new CSV file
         if filtered_tokens:
             writer.writerow(filtered_tokens)
-        
-        
+
 print(f"Processed data has been written to {output_file}")
